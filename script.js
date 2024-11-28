8eb85300b54d677752257ba2c9ab6029d2e099fa
Glen Paul Marquez <angeljanedanila29@gmail.com>
2024-11-28 19:30:23 +0800

    update

--- script.js
+++ script.js
@@ -14,3 +14,10 @@
          navLinks.classList.remove('active');
      });
  });
+
+ // For Submit Student's Enrollment
+ function submitLang(event) {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+    window.location.href = "index.html";
+}
